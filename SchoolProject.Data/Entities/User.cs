5b38d910be889b9b6a0565f3d81c8bc60614692d
abdallah7296 <abdallahabdeltawab2015@gmail.com>
2024-10-13 22:44:47 +0300

    Add table Identity user and add registerService

---
+++ SchoolProject.Data/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace SchoolProject.Data.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public string Address { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+
+    }
+}
--- SchoolProject.infrastructure/Context/ApplicationDbContext.cs
+++ SchoolProject.infrastructure/Context/ApplicationDbContext.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using SchoolProject.Data.Entities;
 using System.Reflection;
-
 namespace SchoolProject.infrastructure.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<User, IdentityRole<int>, int, IdentityUserClaim<int>, IdentityUserRole<int>, IdentityUserLogin<int>, IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public ApplicationDbContext() { }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
@@ -12,6 +13,7 @@ namespace SchoolProject.infrastructure.Context
 
         }
         public DbSet<Department> departments { get; set; }
+        public DbSet<User> users { get; set; }
         public DbSet<Student> students { get; set; }
         public DbSet<Subjects> subjects { get; set; }
         public DbSet<Instructor> instructors { get; set; }
